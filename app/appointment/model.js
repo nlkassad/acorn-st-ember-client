b2f068087d00b5243fdc508505fb93235eb19c67
nlkassad <nlkassad@hotmail.com>
2016-11-08 20:17:35 -0500

    Add display for appointment buttons

--- app/appointment/model.js
+++ app/appointment/model.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   user_id: DS.attr('string'),
   slot_id: DS.attr('string'),
+  editable: DS.attr('boolean'),
   user: DS.belongsTo('user')
 });
