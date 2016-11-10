5b29b9b037fbdefefadd9b1614468c898b3e91a5
nlkassad <nlkassad@hotmail.com>
2016-11-10 07:53:30 -0500

    Roll back on auth conflicts

--- app/appointment/model.js
+++ app/appointment/model.js
@@ -4,6 +4,6 @@ export default DS.Model.extend({
   user_id: DS.attr('string'),
   slot_id: DS.attr('string'),
   editable: DS.attr('boolean'),
-  user: DS.belongsTo('user'),
-  slot: DS.belongsTo('slot'),
+  // user: DS.belongsTo('user'),
+  // slot: DS.belongsTo('slot'),
 });
