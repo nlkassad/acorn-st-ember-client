b27d4c9056d86777976b9ca97e02d1444d468b57
nlkassad <nlkassad@hotmail.com>
2016-11-09 23:09:57 -0500

    Refactor templates for api connection

--- app/appointment/model.js
+++ app/appointment/model.js
@@ -4,5 +4,4 @@ export default DS.Model.extend({
   user_id: DS.attr('string'),
   slot_id: DS.attr('string'),
   editable: DS.attr('boolean'),
-  user: DS.belongsTo('user')
 });
--- app/appointments/route.js
+++ app/appointments/route.js
@@ -1,7 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // localSlots: {},
+  tagName: 'li',
+  classNameBindings: ['appointmentCompleted'],
+  appointmentCompleted: Ember.computed.alias('appointment.done'),
   model () {
     return this.get('store').findAll('appointment');
-  }
+  },
+  actions: {
+    edit () {
+      // localSlots = this.get('store').peekAll('slot');
+      return this.get('store').peekAll('slot');
+    },
+    delete (appointment) {
+      console.log(appointment.id);
+      // let aptId = appointment.id;
+      console.log(appointment.user_id);
+      appointment.destroyRecord();
+      appointment.save();
+      // return this.sendAction('delete', this.get('appointment'));
+      // return this.get('ajax').del(`/sign-out/${this.get('credentials.id')}`)
+      // .finally(() => this.get('credentials').reset());
+    },
+  },
+  // actions:{
+  //   updateValue: function(value) {
+  //     this.set('value', value);
+  //   },
+  // },
 });
