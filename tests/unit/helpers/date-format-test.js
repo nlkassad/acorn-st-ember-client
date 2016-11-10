b51aacb75a49b4c45c66a6fd5328c65bfdab79e4
nlkassad <nlkassad@hotmail.com>
2016-11-10 04:10:43 -0500

    Update appointments data information
    Add updated information for appointments data

---
+++ tests/unit/helpers/date-format-test.js
@@ -0,0 +1,12 @@
+
+import { dateFormat } from 'acorn-st-ember-client/helpers/date-format';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | date format');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = dateFormat([42]);
+  assert.ok(result);
+});
+
